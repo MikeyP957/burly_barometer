89b86468e08e7cf21c97b660f4fa8799cf9f51df
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-04-20 14:52:04 -0700

    data is seeding

---
+++ models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    Exercises: require('./exercises'),
+    Workout: require('./workout')
+
+}
---
+++ models/workout.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    date:{
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Exercises'
+        }
+    ]
+
+})
+
+const Workout = mongoose.model('Workout', WorkoutSchema);
+
+module.exports = Workout;
